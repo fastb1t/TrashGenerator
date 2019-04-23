40378cb1d1288e89a4567ff09846f0bb51993250
Andrew <38564365+fastb1t@users.noreply.github.com>
2019-04-23 17:46:05 +0300

    Added source code

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+
+int main(int argc, char *argv[])
+{
+    std::cout << ">>> TrashGenerator v1.0 <<<\n\n";
+
+    if (argc != 3)
+    {
+        std::cout << "Usage: TrashGenerator [file] [n]\n";
+        return 1;
+    }
+
+    for (int i = 0; i < strlen(argv[2]); i++)
+    {
+        if (!isdigit(argv[2][i]))
+        {
+            std::cerr << "Error! Wrong number.\n";
+            return 1;
+        }
+    }
+    
+    srand(static_cast<size_t>(time(NULL)));
+
+    int k = 0xFFFF;
+
+    std::ofstream ofs;
+    ofs.open(argv[1], std::ios_base::out | std::ios_base::binary | std::ios_base::trunc);
+    if (ofs.is_open())
+    {
+        for (int i = 0; i < atoi(argv[2]); i++)
+        {
+            char c = rand() % (sizeof(char) * 0xFF);
+            ofs.write(reinterpret_cast<const char *>(&c), sizeof(char));
+        }
+        ofs.close();
+        std::cout << "Done!\n";
+    }
+    else
+    {
+        std::cerr << "Error! File '" << argv[1] << "' not created.\n";
+        return 1;
+    }
+    return 0;
+}
