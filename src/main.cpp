61912d05dae368ffe5b80559005c8466e3735c37
Andrew <38564365+fastb1t@users.noreply.github.com>
2019-04-28 15:22:01 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,25 +5,158 @@
 #include <cstdlib>
 #include <ctime>
 
-int main(int argc, char *argv[])
+
+bool isNumber(const char *str)
 {
-    std::cout << ">>> TrashGenerator v1.0 <<<\n\n";
-
-    if (argc != 3)
+    for (int i = 0; i < strlen(str); i++)
     {
-        std::cout << "Usage: TrashGenerator [file] [n bytes]\n";
-        return 1;
-    }
-
-    for (int i = 0; i < strlen(argv[2]); i++)
-    {
-        if (!isdigit(argv[2][i]))
+        if (!isdigit(str[i]))
         {
-            std::cerr << "Error! Wrong number.\n";
-            return 1;
+            return false;
         }
     }
-    
+    return true;
+}
+
+void showHelp()
+{
+    std::cout <<
+        "Usage: TrashGenerator\n"
+        "         -h | --help                     - Show help\n"
+        "         -f | --file   [file]            - Name of file. if you specified more than one file,\n"
+        "                                           then specify [%] in the file name to indicate the\n"
+        "                                           file number, otherwise only one file will be\n"
+        "                                           created. For example: filename[%].dat\n"
+        "         -n | --files  [n]               - n files\n"
+        "         -b | --bytes  [n]               - n bytes\n"
+        "         -s | --size   [fixed | random]  - Size of file. if --size == fixed, then the file will\n"
+        "                                           be created with a fixed size, and if --size == random,\n"
+        "                                           then the file will be created with a random size whose\n"
+        "                                           maximum value will be what you specified in --bytes\n"
+    ;
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << ">>> TrashGenerator v2.0 <<<\n\n";
+
+    if (argc == 1)
+    {
+        showHelp();
+        return 0;
+    }
+
+    enum SIZE_M {
+        SIZE_ERROR,
+        RANDOM_SIZE,
+        FIXED_SIZE
+    };
+
+    std::string file;
+    long nfiles = 1;
+    long nbytes = 0;
+    SIZE_M size_m = SIZE_ERROR;
+
+    for (int i = 0; i < argc; i++)
+    {
+        if (!strcmp(argv[i], "-h") || !strcmp(argv[i], "--help"))
+        {
+            showHelp();
+            return 0;
+        }
+
+        if (!strcmp(argv[i], "-f") || !strcmp(argv[i], "--file"))
+        {
+            if (i + 1 < argc)
+            {
+                file = argv[i + 1];
+            }
+            else
+            {
+                std::cout << "Error! File name not specified.\n";
+                return 1;
+            }
+        }
+
+        if (!strcmp(argv[i], "-n") || !strcmp(argv[i], "--files"))
+        {
+            if (i + 1 < argc)
+            {
+                if (!isNumber(argv[i + 1]))
+                {
+                    std::cerr << "Error! Wrong number of files.\n";
+                    return 1;
+                }
+                else
+                {
+                    nfiles = atol(argv[i + 1]);
+                    if (nfiles <= 0)
+                    {
+                        std::cerr << "Error! Wrong number of files.\n";
+                        return 1;
+                    }
+                }
+            }
+            else
+            {
+                std::cerr << "Error! Number of files not specified.\n";
+                return 1;
+            }
+        }
+
+        if (!strcmp(argv[i], "-b") || !strcmp(argv[i], "--bytes"))
+        {
+            if (i + 1 < argc)
+            {
+                if (!isNumber(argv[i + 1]))
+                {
+                    std::cerr << "Error! Wrong number of bytes.\n";
+                    return 1;
+                }
+                else
+                {
+                    nbytes = atol(argv[i + 1]);
+                    if (nbytes < 0)
+                    {
+                        std::cerr << "Error! Wrong number of bytes.\n";
+                        return 1;
+                    }
+                }
+            }
+            else
+            {
+                std::cerr << "Error! Number of bytes not specified.\n";
+                return 1;
+            }
+        }
+
+        if (!strcmp(argv[i], "-s") || !strcmp(argv[i], "--size"))
+        {
+            if (i + 1 < argc)
+            {
+                if (!strcmp(argv[i + 1], "random"))
+                {
+                    size_m = RANDOM_SIZE;
+                }
+                else if (!strcmp(argv[i + 1], "fixed"))
+                {
+                    size_m = FIXED_SIZE;
+                }
+                else
+                {
+                    std::cerr << "Error! .\n";
+                    return 1;
+                }
+            }
+            else
+            {
+                std::cerr << "Error! .\n";
+                return 1;
+            }
+        }
+    }
+
+    /*
     srand(static_cast<size_t>(time(NULL)));
 
     std::ofstream ofs;
@@ -43,5 +176,6 @@ int main(int argc, char *argv[])
         std::cerr << "Error! File '" << argv[1] << "' not created.\n";
         return 1;
     }
+    */
     return 0;
 }
